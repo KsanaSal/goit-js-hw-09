c5e8a18fa7b3eeb948c0da988f7bbfb018578569
KsanaSal <oasalivon@gmail.com>
2022-08-10 22:29:52 +0300

    Solution task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,18 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+let timerId = null;
+startBtn.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startBtn.disabled = true;
+});
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  startBtn.disabled = false;
+});
